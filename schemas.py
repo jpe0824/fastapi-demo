abd2d9a589538a062daa06b8dce15841b185b268
jpe0824 <jason.e24@gmail.com>
2023-09-25 14:10:16 -0600

    fast-api tutorial

---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+import json
+
+
+class CarInput(BaseModel):
+    size: str
+    fuel: str | None = "electric"
+    doors: int
+    transmission: str | None = "auto"
+
+class CarOutput(CarInput):
+    id: int
+
+def load_db() -> list[CarOutput]:
+    with open("cars.json") as f:
+        return [CarOutput.model_validate(obj) for obj in json.load(f)]
+
+
+def save_db(cars: list[CarInput]):
+    with open("cars.json", "w") as f:
+        json.dump([car.model_dump() for car in cars], f, indent=4)
